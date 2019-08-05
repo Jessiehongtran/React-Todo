9c4db83a56f1b0ae7e6f9d1c88a38d86b4907655
Your Name <you@example.com>
2019-08-05 15:26:43 -0400

    set up state

--- src/App.js
+++ src/App.js
@@ -20,21 +20,26 @@ const TodoData = [
 
 
 
-
-
-
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
   
+  constructor(){
+    super();
+    this.state = {
+      name: 'Hong',
+      Todo : TodoData
+    }
+    console.log(this.state)
+  }
   
   
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
-        <div>I am ready</div>
+        <h2>Welcome to {this.state.name}'s Todo App!</h2>
+        
       </div>
     );
   }
