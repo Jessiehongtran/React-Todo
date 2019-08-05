f4d7e07f7d0f9de233b13b41b7352e88e2dfcd8e
Your Name <you@example.com>
2019-08-05 15:21:33 -0400

    Created todo data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
 import React from 'react';
 
+const TodoData = [
+  {
+    task: 'Grocery shopping',
+    id: 123,
+    completed: false
+  },
+  {
+    task: 'Finish Lambda Sprint',
+    id: 1234,
+    completed: false
+  },
+  {
+    task: 'Gardening',
+    id: 12345,
+    completed: true
+  }
+]
+
+
+
+
+
+
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  
+  
+  
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+        <div>I am ready</div>
       </div>
     );
   }
